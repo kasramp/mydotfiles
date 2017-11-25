e2526e8dc121316041f0f2a7e8a4063310779351
kasramp <kasra_mp@yahoo.com>
2017-11-26 02:27:19 +0800

    Add Scratchpad support
    Four scratchpad added.
    - Vim shortcut note
    - Nerdtree shortcut note
    - Tmux shortcut note
    - Handy terminator for generic tasks (music, daemon)

---
+++ .config/i3/widget_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp '/home/kixz/Documents/tmux_101.md' '/tmp/tmux.md'
+cp '/home/kixz/Documents/vim_nerd_tree.txt' '/tmp/nerdtree.md'
+cp '/home/kixz/Documents/markdown/vim_tutor/vim_tutor.md' '/tmp/vim.md'

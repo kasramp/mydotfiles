1a7768eb9d427d8800d6639194f5284a9c5b046a
kasramp <kasra_mp@yahoo.com>
2019-01-15 08:00:05 +0100

    Add monaco font installer

---
+++ monaco_font_installer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script install the Monaco font for the logged in user
+# and updates Urxvt terminal font to Monaco.
+# This happens by updating .Xresources file.
+# So user doesn't need to restart the system.
+
+`mkdir -p ~/.fonts/`
+`cp font/Monaco-Linux.ttf ~/.fonts`
+`fc-cache -fv`
+`echo '! Change the font to Monaco' >> ~/.Xresource`
+`echo 'URxvt*font: xft:monaco:size=11' >> ~/.Xresources`
+`xrdb ~/.Xresources`
+
+echo -e "\n"
+ehco -e "\n"
+echo -e "Monaco font installation has finished\n"
+echo -e "Close your terminal and open again to see the effect\n"
+echo "Enjoy using Monaco font!!!"

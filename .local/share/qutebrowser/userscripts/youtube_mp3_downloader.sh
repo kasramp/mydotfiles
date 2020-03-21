a1baef12abf77f8f6d31ff9c941a51863b9746bb
kasramp <kasra_mp@yahoo.com>
2020-03-21 11:48:32 +0100

    minor clean up on qutebrowser config

--- .local/share/qutebrowser/userscripts/youtube_mp3_downloader.sh
+++ .local/share/qutebrowser/userscripts/youtube_mp3_downloader.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-alias mp3-download='youtube-dl -x --audio-format mp3  --audio-quality  0'
 
-(youtube-dl --output "$QUTE_DOWNLOAD_DIR/%(title)s.%(ext)s" -x --audio-format mp3  --audio-quality  0 $QUTE_URL && 
+(youtube-dl --output "$QUTE_DOWNLOAD_DIR/%(title)s.%(ext)s" -x --audio-format mp3 --audio-quality 0 $QUTE_URL && 
     notify-send "YouTube mp3 download completed!" "$QUTE_TITLE") || exit 0

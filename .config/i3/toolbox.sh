68a4803361db719f288f8b9005bb22dac0b8abe1
kasramp <kasra_mp@yahoo.com>
2020-05-16 00:08:22 +0200

    Add Solaar and upgrade Jetbrains Toolbox version

--- .config/i3/toolbox.sh
+++ .config/i3/toolbox.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-exec /home/kixz/Private/Software/Programfiles/jetbrains-toolbox-1.11.4231/jetbrains-toolbox --minimize
+# Do not upgrade to `1.17`, it breaks and minimize won't work.
+# No icon will show in tray
+exec /home/kixz/Private/Software/Programfiles/jetbrains-toolbox-1.14.5179/jetbrains-toolbox --minimize
